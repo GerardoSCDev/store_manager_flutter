ed70076d3d336a3ec623cd3c54660ee0b79756de
Gerardo Santillan <gerardo.sc.dev@gmail.com>
2025-04-25 08:06:08 -0600

    form for detail product

--- lib/src/modules/inventory/formProducts/views/form_products_view.dart
+++ lib/src/modules/inventory/formProducts/views/form_products_view.dart
@@ -7,15 +7,70 @@ class FormProductsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
+      width: double.infinity,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            height: 10,
-            width: 10,
-            color: Colors.black,
-            padding: EdgeInsets.only(left: 10, right: 10),
+            height: 5,
+            width: 150,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: Text(
+              "Actualizacion",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: SingleChildScrollView(
+                child: Column(
+                  spacing: 20,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "CP",
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Nombre",
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Cantidad",
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Presio",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(bottom: 20, left: 30, right: 30),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(onPressed: () {}, child: Text("Agregar")),
+            ),
           ),
         ],
       ),
